d0d275d441c0cbd0f1605cfe1a798290c453e7dc
csm892 <1619212193@qq.com>
2021-04-18 16:18:50 +0800

    Initial commit

---
+++ src/com/javapandeng/po/CarDto.java
@@ -0,0 +1,23 @@
+package com.javapandeng.po;
+
+public class CarDto {
+    //前台传来购物的id和数量
+    private Integer id;
+    private Integer num;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Integer getNum() {
+        return num;
+    }
+
+    public void setNum(Integer num) {
+        this.num = num;
+    }
+}
